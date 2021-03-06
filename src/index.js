47f46f800133e95049904154f1d3829e0dac9948
Bea <beatriztattooer@gmail.com>
2021-03-05 23:10:23 -0300

    It's working :) And I thought it's beautiful hahahah

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
 
-const oddOrEven = e => e % 2 == 0;
+// const oddOrEven = e => e % 2 == 0;
 
-let result = numbers.map(oddOrEven);
+// let result = numbers.map(oddOrEven);
 
-console.log(result);
+// console.log(result);
+
+
+function oddOrEven(){
+
+    var number = document.getElementById('oddOrEven').value;
+    var optionOdd = document.getElementsByClassName('option--odd');
+    var optionEven = document.getElementsByClassName('option--even');
+
+    if( number % 2 == 0){
+        optionEven[0].classList.remove('d-block');
+        optionOdd[0].classList.add('d-block');
+    }else{
+        optionOdd[0].classList.remove('d-block');
+        optionEven[0].classList.add('d-block');
+    }
+
+}
